25362855c82428c12b9d35ba9ea2363146c42092
bishnunathbd <bishnunath774@gmail.com>
2021-04-09 17:51:02 +0600

    38-5-install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import NoMatch from './components/NoMatch/NoMatch';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/home'>
+          <Home></Home>
+        </Route>
+        <Route exact path='/'>
+          <Home></Home>
+        </Route>
+        <Route path='*'>
+          <NoMatch></NoMatch>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
